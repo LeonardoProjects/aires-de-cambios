3e0ee942013246404a023029767ba8c9576f1653
axelCoronel <lexel61@gmail.com>
2024-09-10 14:53:41 -0300

    [APImode] HourlyWeatherData + ForecastWeatherData classes creation

---
+++ app/Services/ForecastWeatherData.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Services;
+
+class ForecastWeatherData
+{
+    private $localtime;
+    private $firstDate = [];
+    private $secondDate = [];
+
+    public function __construct($localtime, $firstDate, $secondDate) {
+        $this->localtime = $localtime;
+        $this->firstDate = $this->generateDateData($firstDate);
+        $this->secondDate = $this->generateDateData($secondDate);
+    }
+
+    private function generateDateData($DatehourlyData)
+    {
+        $hourlyCollection = [];
+        foreach ($DatehourlyData as $index => $data) {
+            $hourlyCollection[] = new HourlyWeatherData(
+                $index,
+                $data['wind_kph'],
+                $data['humidity'],
+                $data['condition']
+            );
+        }
+        return $hourlyCollection;
+    }
+
+    public function getLocaltime()
+    {
+        return $this->localtime;
+    }
+
+    public function getFirstDateData()
+    {
+        return $this->firstDate;
+    }
+
+    public function getSecondDateData()
+    {
+        return $this->secondDate;
+    }
+
+    
+}
