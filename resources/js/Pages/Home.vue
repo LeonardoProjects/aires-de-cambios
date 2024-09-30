2534f30f4fd5cbb22a4afc46457f295af7b8448d
axelCoronel <lexel61@gmail.com>
2024-09-30 18:55:26 -0300

    [home] commit de respaldo

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,7 +1,20 @@
 <template>
-    <div>
-        HOME
-    </div>
+    <section class="hero-section">
+        <div>
+            <h1>Aires de cambios</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed corrupti iure quas fugit minima ipsam unde maxime dignissimos autem! Recusandae neque magnam numquam doloremque ullam sint officiis inventore ea quo?</p>
+            <button>Probar la aplicacion</button>
+        </div>
+    </section>
+    <section class="app-info">
+        <p>usuarios activos</p>
+        <div>grafica 1</div>
+        <p>usuarios registrados</p>
+        <div>grafica 2</div>
+    </section>
+    <section class="about-us">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti rem minus dignissimos libero sint atque, delectus odio eaque iusto sed quasi ea amet laborum illum. Amet eos neque eum magni.</p>
+    </section>
 </template>
 
 <script>
