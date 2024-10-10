6cc03112a5abc7939be6cb889331034a7a387a39
axelCoronel <lexel61@gmail.com>
2024-10-10 01:23:16 -0300

    [home] Footer fix + map final implementation

--- resources/js/Components/EnvironmentsPerCountryChart.vue
+++ resources/js/Components/EnvironmentsPerCountryChart.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
-        <div id="map2" style="height: 500px; width: 1000px;"></div>
-        <button @click="crearMapa">Crear Mapa</button>
+        <div id="map2" style="height: 500px; width: 1000px"></div>
     </div>
 </template>
 
@@ -13,54 +12,148 @@ export default {
     name: "EnvironmentsPerCountryChart",
     setup() {
         const map2 = ref(null);
-        const maxAmbientes = ref(10);
+        const maxAmbientes = ref(0);
+        const roundToNearestPowerOfTen = (num) => {
+            if (num < 10) return 10;
+            return Math.pow(10, Math.floor(Math.log10(num)));
+        };
+
+        const getColor = (total, max) => {
+            if (max === 0) return "lightgray";
+            const intensity = total / max;
+
+            const startColor = [255, 255, 178];
+            const endColor = [255, 69, 0];
+
+            const r = Math.floor(
+                startColor[0] + intensity * (endColor[0] - startColor[0])
+            );
+            const g = Math.floor(
+                startColor[1] + intensity * (endColor[1] - startColor[1])
+            );
+            const b = Math.floor(
+                startColor[2] + intensity * (endColor[2] - startColor[2])
+            );
+
+            return `rgba(${r}, ${g}, ${b}, 0.7)`;
+        };
 
         const crearMapa = () => {
             if (map2.value) {
-                return; 
+                return;
             }
-            map2.value = L.map("map2", { preferCanvas: true }).setView([20, 0], 2);
+
+            map2.value = L.map("map2", {
+                minZoom: 2,
+                maxZoom: 4,
+                worldCopyJump: false,
+                maxBounds: [
+                    [40.712, -74.227],
+                    [40.774, -74.125],
+                ],
+                maxBoundsViscosity: 1,
+            }).setView([20, 0], 2);
+
+            const southWest = L.latLng(-85, -180);
+            const northEast = L.latLng(85, 180);
+            const bounds = L.latLngBounds(southWest, northEast);
+            map2.value.setMaxBounds(bounds);
 
             L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
                 maxZoom: 18,
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                attribution:
+                    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
             }).addTo(map2.value);
-
-            axios.get('/countries-data')
-                .then(response => {
+            axios
+                .get("/countries-data")
+                .then((response) => {
                     const ambientesPorPais = response.data;
 
-                    axios.get('/country-data/ne_10m_admin_0_countries_arg.json')
-                        .then(geoJsonResponse => {
-                            L.geoJSON(geoJsonResponse.data, {
-                                style: feature => {
-                                    const pais = feature.properties.ADMIN;
-                                    const ambiente = ambientesPorPais.find(a => a.pais === pais);
+                    const maxAmbientesCalculado = Math.max(
+                        ...ambientesPorPais.map((a) => a.total)
+                    );
+                    maxAmbientes.value = roundToNearestPowerOfTen(
+                        maxAmbientesCalculado
+                    );
 
-                                    let fillColor = 'lightgray';
+                    axios
+                        .get("/country-data/ne_10m_admin_0_countries_arg.json")
+                        .then((geoJsonResponse) => {
+                            L.geoJSON(geoJsonResponse.data, {
+                                style: (feature) => {
+                                    const pais = feature.properties.ADMIN;
+                                    const ambiente = ambientesPorPais.find(
+                                        (a) => a.pais === pais
+                                    );
+
+                                    let fillColor = "lightgray";
                                     if (ambiente) {
                                         const total = ambiente.total;
-                                        const intensity = Math.min(total / maxAmbientes.value, 1);
-                                        fillColor = `rgba(0, 0, 255, ${intensity})`; 
+                                        fillColor = getColor(
+                                            total,
+                                            maxAmbientes.value
+                                        );
                                     }
 
                                     return {
-                                        color: 'black',
+                                        color: "black",
                                         fillColor: fillColor,
                                         fillOpacity: 0.7,
                                         weight: 1,
                                     };
-                                }
+                                },
+
+                                onEachFeature: (feature, layer) => {
+                                    const pais = feature.properties.ADMIN;
+                                    const ambiente = ambientesPorPais.find(
+                                        (a) => a.pais === pais
+                                    );
+                                    const totalAmbientes = ambiente
+                                        ? ambiente.total
+                                        : 0;
+
+                                    layer.bindPopup(`
+                                        <strong>País:</strong> ${pais}<br>
+                                        <strong>Ambientes:</strong> ${totalAmbientes}
+                                    `);
+                                },
                             }).addTo(map2.value);
+
+                            // Añadir leyenda
+                            var legend = L.control({ position: "bottomright" });
+
+                            legend.onAdd = function () {
+                                const div = L.DomUtil.create(
+                                    "div",
+                                    "info legend"
+                                );
+                                const gradient =
+                                    "linear-gradient(to right, rgba(255, 237, 160, 0.7), rgba(255, 69, 0, 0.7))";
+                                div.innerHTML += `<div style="width: 200px; height: 20px; background: ${gradient};"></div>`;
+                                div.innerHTML += `<div style="display: flex; justify-content: space-between;">
+                        <span>0</span><span>${maxAmbientes.value}</span>
+                      </div>`;
+
+                                return div;
+                            };
+
+                            legend.addTo(map2.value);
                         })
-                        .catch(error => console.error('Error al cargar el GeoJSON:', error));
+                        .catch((error) =>
+                            console.error("Error al cargar el GeoJSON:", error)
+                        );
                 })
-                .catch(error => console.error('Error al obtener datos de países:', error));
+                .catch((error) =>
+                    console.error("Error al obtener datos de países:", error)
+                );
         };
 
         return {
             crearMapa,
         };
+    },
+    mounted() {
+        this.crearMapa();
     },
 };
 </script>
@@ -69,5 +162,18 @@ export default {
 #map2 {
     height: 500px;
     width: 1000px;
+}
+
+.info.legend {
+    background-color: white;
+    padding: 6px;
+    font-size: 12px;
+    line-height: 18px;
+    color: #333;
+    text-align: center;
+}
+
+.info.legend div {
+    margin: 4px 0;
 }
 </style>
